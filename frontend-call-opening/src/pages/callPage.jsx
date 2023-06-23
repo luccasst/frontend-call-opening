3824024dad82e2f1dfa10735f994bf114578408b
luccasst <lucas.c.r.f@hotmail.com>
2023-06-23 15:42:23 -0300

    add loading e sucessMessage when registering a new call

--- frontend-call-opening/src/pages/callPage.jsx
+++ frontend-call-opening/src/pages/callPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useContext } from 'react';
 import NavBar from '../components/navBar';
-import {CallContext} from '../context/context';
+import { CallContext } from '../context/context';
 import './callPageStyle.css';
 
 const CallPage = () => {
@@ -10,46 +10,46 @@ const CallPage = () => {
   const [priority, setPriority] = useState('');
   const { user, setUser, name, setName } = useContext(CallContext);
 
-  const [ calls, setCalls ] = useState([]);
+  const [calls, setCalls] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
 
   const handleDelete = (callId) => {
-      const token = localStorage.getItem('token');
+    const token = localStorage.getItem('token');
 
-      fetch(`http://192.168.0.39:3010/call/${callId}`, {
-        method: 'DELETE',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: token,
-        },
-      })
-      .then(response => response.json())
-    .then(data => {
-      console.log('Chamado excluído:', data);
-      setCalls(prevCalls => prevCalls.filter(call => call.id !== callId));
+    fetch(`http://192.168.0.39:3010/call/${callId}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: token,
+      },
     })
-    .catch(error => console.error('Erro ao excluir chamado:', error));
-};
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Chamado excluído:', data);
+        setCalls((prevCalls) => prevCalls.filter((call) => call.id !== callId));
+      })
+      .catch((error) => console.error('Erro ao excluir chamado:', error));
+  };
 
-useEffect(() => {
-  const userData = JSON.parse(localStorage.getItem('user'));
-  setUser(userData);
-}, [setUser])
+  useEffect(() => {
+    const userData = JSON.parse(localStorage.getItem('user'));
+    setUser(userData);
+  }, [setUser]);
 
-  
-useEffect(() => {
-  const token = localStorage.getItem('token');
-  fetch('http://192.168.0.39:3010/call', {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: token,
-    }
-  })
-  .then(response => response.json())
-  .then(data => setCalls(data.calls))
-  .catch(error => console.error('Erro ao buscar chamados:', error));
-}, [])
-  
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    fetch('http://192.168.0.39:3010/call', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: token,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => setCalls(data.calls))
+      .catch((error) => console.error('Erro ao buscar chamados:', error));
+  }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -64,6 +64,8 @@ useEffect(() => {
 
     const token = localStorage.getItem('token');
     console.log(token);
+
+    setIsLoading(true);
 
     fetch('http://192.168.0.39:3010/call', {
       method: 'POST',
@@ -88,10 +90,13 @@ useEffect(() => {
         } else {
           console.log('Usuário não está definido.');
         }
-
+        setShowSuccessMessage(true);
       })
       .catch((error) => {
         console.error('Erro ao cadastrar chamado', error.response);
+      })
+      .finally(() => {
+        setIsLoading(false);
       });
 
     setTitle('');
@@ -100,49 +105,66 @@ useEffect(() => {
     setPriority('');
   };
 
+  useEffect(() => {
+    if (showSuccessMessage) {
+      setShowSuccessMessage(false);
+      window.location.reload();
+    }
+  }, [showSuccessMessage]);
+
   return (
     <div>
       <NavBar user={user?.name} />
-
-      <div className='H1'>
+      <div className="loadingContainer">
+      {isLoading && <div className="loading">Carregando...</div>}
+      {showSuccessMessage && (
+        <div className="successMessage">Chamado cadastrado com sucesso!</div>
+      )}
+    </div>
+      <div className="H1">
         <h1>BEM VINDO AO CALL OPENING</h1>
       </div>
-      <div className='H3'>
+      <div className="H3">
         <h3>Abra seu chamado :)</h3>
       </div>
-      <form onSubmit={handleSubmit} className='formContainer'>
-        <div className='formGroup'>
-          <label htmlFor='title'>Título:</label>
+      <form onSubmit={handleSubmit} className="formContainer">
+        <div className="formGroup">
+          <label htmlFor="title">Título:</label>
           <input
-            type='text'
-            id='title'
+            type="text"
+            id="title"
             value={title}
             onChange={(e) => setTitle(e.target.value)}
           />
         </div>
-        <div className='formGroup'>
-          <label htmlFor='comment'>Comentário:</label>
+        <div className="formGroup">
+          <label htmlFor="comment">Comentário:</label>
           <input
-            type='text'
-            id='comment'
+            type="text"
+            id="comment"
             value={comment}
             onChange={(e) => setComment(e.target.value)}
           />
         </div>
-        <div className='formGroup'>
-          <label htmlFor='status'>Status:</label>
-          <input
-            type='text'
-            id='status'
+        <div className="formGroup">
+          <label htmlFor="status">Status:</label>
+          <select
+            type="text"
+            id="status"
             value={status}
             onChange={(e) => setStatus(e.target.value)}
-          />
+            >
+              <option value="">Selecione</option>
+            <option value="início">Início</option>
+            <option value="tratamento">Tratamento</option>
+            <option value="concluído">Concluído</option>
+          </select>
         </div>
-        <div className='formGroup'>
-          <label htmlFor='priority'>Prioridade:</label>
+        <div className="formGroup">
+          <label htmlFor="priority">Prioridade:</label>
           <select
-            type='text'
-            id='priority'
+            type="text"
+            id="priority"
             value={priority}
             onChange={(e) => setPriority(e.target.value)}
           >
@@ -152,64 +174,63 @@ useEffect(() => {
             <option value="alta">Alta</option>
           </select>
         </div>
-        <button type='submit'>Cadastrar chamado</button>
+        <button type="submit">Cadastrar chamado</button>
       </form>
 
-    <div className="getCalls">
-      <div className="column">
-    <h3>Título</h3>
-    {calls.map(call => (
-      <div key={call.id} className="callItem">
-        <p>{call.title}</p>
+      <div className="getCalls">
+        <div className="column">
+          <h3>Título</h3>
+          {calls.map((call) => (
+            <div key={call.id} className="callItem">
+              <p>{call.title}</p>
+            </div>
+          ))}
+        </div>
+
+        <div className="column">
+          <h3>Comentário</h3>
+          {calls.map((call) => (
+            <div key={call.id} className="callItem">
+              <p>{call.comment}</p>
+            </div>
+          ))}
+        </div>
+
+        <div className="column">
+          <h3>Status</h3>
+          {calls.map((call) => (
+            <div key={call.id} className="callItem">
+              <p>{call.status}</p>
+            </div>
+          ))}
+        </div>
+
+        <div className="column">
+          <h3>Prioridade</h3>
+          {calls.map((call) => (
+            <div key={call.id} className="callItem">
+              <p>{call.priority}</p>
+            </div>
+          ))}
+        </div>
+
+        <div className="column">
+          <h3>Usuário</h3>
+          {calls.map((call) => (
+            <div key={call.id} className="callItem">
+              <p>{call && user?.name && user?.name.split(' ')[0]}</p>
+            </div>
+          ))}
+        </div>
+        <div className="column">
+          <h3>Excluir</h3>
+          {calls.map((call) => (
+            <div key={call.id} className="callItemButtom">
+              <button onClick={() => handleDelete(call.id)}>Excluir</button>
+            </div>
+          ))}
+        </div>
       </div>
-    ))}
-  </div>
-
-  <div className="column">
-    <h3>Comentário</h3>
-    {calls.map(call => (
-      <div key={call.id} className="callItem">
-        <p>{call.comment}</p>
-      </div>
-    ))}
-  </div>
-
-  <div className="column">
-    <h3>Status</h3>
-    {calls.map(call => (
-      <div key={call.id} className="callItem">
-        <p>{call.status}</p>
-      </div>
-    ))}
-  </div>
-
-  <div className="column">
-    <h3>Prioridade</h3>
-    {calls.map(call => (
-      <div key={call.id} className="callItem">
-        <p>{call.priority}</p>
-      </div>
-    ))}
-  </div>
-
-  <div className="column">
-  <h3>Usuário</h3>
-  {calls.map(call => (
-    <div key={call.id} className="callItem">
-      <p>{call && user?.name && user?.name.split(' ')[0]}</p>
-    </div>
-  ))}
-</div>
-<div className="column">
-  <h3>Excluir</h3>
-  {calls.map(call => (
-    <div key={call.id} className="callItemButtom">
-      <button onClick={() => handleDelete(call.id)}>Excluir</button>
-    </div>
-  ))}
-</div>
-</div>
-
     </div>
   );
 };
